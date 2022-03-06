b6800ccd3b505b1fd6d58b7e41298d279a535d19
Wolfgang <wolfgang.spindeler@gmail.com>
2022-03-06 22:11:37 +0100

    v 0.0

---
+++ src/series.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class ControlledInterventionSeries:
+    def __init__(self, series, control_series, intervention_index):
+        self.series = series
+        self.control_series = control_series
+        self.intervention_index = intervention_index
+
+    def means(self):
+        means = {}
+        series_before = self.series[:self.intervention_index]
+        series_after = self.series[self.intervention_index:]
+        means["series"] = np.array([self.series.mean(), series_before.mean(), series_after.mean()])
+
+        control_series_before = self.control_series[:self.intervention_index]
+        control_series_after = self.control_series[self.intervention_index:]
+        means["control_series"] = np.array(
+            [self.control_series.mean(), control_series_before.mean(), control_series_after.mean()]
+        )
+
+        return means
